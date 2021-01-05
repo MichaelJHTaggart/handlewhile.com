ae72a773814ce2ca952ac67168d3e356ba33d76c
Michael Taggart <mtaggarthawk@gmail.com>
2021-01-05 16:41:54 -0700

    user get requests finished

---
+++ db/seeding.sql
@@ -0,0 +1,25 @@
+/*
+Add these to postman
+*/
+
+{
+    "username": "pikas",
+    "email": "pikas@pikas.com",
+    "password": "pikas"
+}
+
+{
+    "username": "blast",
+    "email": "blast@blast.com",
+    "password": "blast"
+}
+
+/*
+Add this to pgweb
+*/
+
+INSERT INTO timed_events
+(time, name, user_id)
+VALUES
+(1, 'what?', 3), (2, 'when?', 3), (3, 'where?', 3), (4, 'how?', 4), (5, 'why?', 4);
+
